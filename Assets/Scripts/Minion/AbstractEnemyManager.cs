36ffdf9602aa186d034f92f7f62a6e9ae83eb050
Andrey-PC\Andrey <Pirynas@mail.ru>
2021-01-31 19:40:34 +0700

    создание фабрики менеджеров

---
+++ Assets/Scripts/Minion/AbstractEnemyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arcade
+{
+    /// <summary>
+    /// Абстрактный менеджер противников
+    /// </summary>
+    public abstract class AbstractEnemyManager : MonoBehaviour
+    {
+        /// <summary>
+        /// Инициализация менеджера
+        /// </summary>
+        /// <param name="playerPosition"></param>
+        /// <returns></returns>
+        public abstract AbstractEnemyManager Init(Transform playerPosition);
+
+        /// <summary>
+        /// Переключение работы менеджера спавнера
+        /// </summary>
+        /// <param name="value"></param>
+        public abstract void EnableSpawn(bool value);
+    }
+}
---
+++ Assets/Scripts/Minion/EnemyManagerFactory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arcade
+{
+    /// <summary>
+    /// Фабрика менеджеров противников
+    /// </summary>
+    public class EnemyManagerFactory : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform playerPosition;
+
+        private List<AbstractEnemyManager> factories = new List<AbstractEnemyManager>();
+            
+        private void Start()
+        {
+            GameObject factory = new GameObject();
+
+            AbstractEnemyManager enemyFactory = factory.AddComponent<EnemyManager>();
+            enemyFactory.Init(playerPosition);
+
+            factories.Add(enemyFactory);            
+        }
+
+        /// <summary>
+        /// Включить или выключить спавнеры
+        /// </summary>
+        /// <param name="balue"></param>
+        public void EnableProcess(bool balue)
+        {
+            factories.ForEach(x => x.EnableSpawn(balue));
+        }
+
+    }
+}
